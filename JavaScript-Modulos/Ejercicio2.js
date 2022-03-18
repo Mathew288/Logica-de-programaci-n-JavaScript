a663dc780d0cc0937404e3a0518c9e7cc9685ec4
Richard Guahc <guachrichard@gmail.com>
2022-03-18 02:45:20 -0500

    Manejo de las cadenas de texto

--- JavaScript-Modulos/Ejercicio2.js
+++ JavaScript-Modulos/Ejercicio2.js
@@ -1,21 +1,23 @@
-/*5) Programa una función que invierta las palabras de una cadena de texto
-, pe. miFuncion("Hola Mundo") devolverá "odnuM aloH". */
+/* 
+!5) Programa una función que invierta las palabras de una cadena de texto
+!, pe. miFuncion("Hola Mundo") devolverá "odnuM aloH". */
 
 export const invertirCadena = (cadena = "") => {
     if(cadena === ""){
         console.info("La cadena está vacia");
     } else {
         return cadena.split("").reverse().join("");
-        // Split("") toma todos los caracteres de la cadena y los transforma en 
-        // un array. reverse() invierte el orden del array. join("") unirá
-        // todos los caracteres invertidos por la funcion reverse 
+        
+        //TODO: Split("") toma todos los caracteres de la cadena y los transforma en 
+        //TODO: un array. reverse() invierte el orden del array. join("") unirá
+        //TODO: todos los caracteres invertidos por la funcion reverse 
     }
 }
 
 
-// 6) Programa una función para contar el número de veces que se repite una 
-// palabra en un texto largo, pe. miFuncion("hola mundo adios mundo", "mundo") 
-// devolverá 2.
+// !6) Programa una función para contar el número de veces que se repite una 
+// !palabra en un texto largo, pe. miFuncion("hola mundo adios mundo", "mundo") 
+// !devolverá 2.
 
 export const palabrasRepetidas = (texto,palabraRepetida) =>{
     let expresionRegular = new RegExp(palabraRepetida, "ig");
@@ -23,9 +25,9 @@ export const palabrasRepetidas = (texto,palabraRepetida) =>{
 }
 
 
-// 7) Programa una función que valide si una palabra o frase dada, es un 
-// palíndromo (que se lee igual en un sentido que en otro), pe. mifuncion("Salas") 
-// devolverá true.
+// !7) Programa una función que valide si una palabra o frase dada, es un 
+// !palíndromo (que se lee igual en un sentido que en otro), pe. mifuncion("Salas") 
+// !devolverá true.
 
 export const esPalindromo = (palabra) => {
     if(palabra===invertirCadena(palabra)){
@@ -35,9 +37,9 @@ export const esPalindromo = (palabra) => {
     }
 }
 
-// 8) Programa una función que elimine cierto patrón de caracteres de un texto 
-// dado, pe. miFuncion("xyz1, xyz2, xyz3, xyz4 y xyz5", "xyz")
-// devolverá  "1, 2, 3, 4 y 5.
+//! 8) Programa una función que elimine cierto patrón de caracteres de un texto 
+// !dado, pe. miFuncion("xyz1, xyz2, xyz3, xyz4 y xyz5", "xyz")
+// !devolverá  "1, 2, 3, 4 y 5.
 
 export const eliminarCaracteres = (texto,patron_a_eliminar) => {
     
